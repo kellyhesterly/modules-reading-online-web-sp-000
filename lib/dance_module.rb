16c71765964e722d1c8d5f32c36289ac45ddbd32
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-17 20:51:59 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,4 @@
-module FancyDance
-  module InstanceMethods
-
+module Dance
   def twirl
     "I'm twirling!"
   end
@@ -15,11 +13,5 @@ module FancyDance
 
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
-  end
-end
-
-module metadata
-  "This class produces objects that love to dance."
-    end
   end
 end
